693f139a6e415c1a1e0e7704344293eb6e489e54
mihkeltina <mihkeltina@gmail.com>
2025-03-11 17:53:48 +0200

    Add flights, add crossorigin, fix random seat

--- demo/src/main/java/com/example/demo/APIController.java
+++ demo/src/main/java/com/example/demo/APIController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
@@ -11,33 +12,50 @@ import java.util.Map;
 
 @RestController
 public class APIController {
-
+    @CrossOrigin
     @GetMapping("/flights")
     public List<Map<String, String>> getFlights() {
         Map<String, String> flight1 = new HashMap<>();
         flight1.put("id", "1");
         flight1.put("from", "Tallinn");
         flight1.put("to", "Paris");
+        flight1.put("departure", "10:30");
 
         Map<String, String> flight2 = new HashMap<>();
         flight2.put("id", "2");
         flight2.put("from", "Tallinn");
         flight2.put("to", "Helsinki");
+        flight2.put("departure", "14:45");
+
+        Map<String, String> flight3 = new HashMap<>();
+        flight3.put("id", "3");
+        flight3.put("from", "Tallinn");
+        flight3.put("to", "Rome");
+        flight3.put("departure", "16:45");
+
+        Map<String, String> flight4 = new HashMap<>();
+        flight4.put("id", "4");
+        flight4.put("from", "Milan");
+        flight4.put("to", "Helsinki");
+        flight4.put("departure", "19:15");
 
         List<Map<String, String>> flights = new ArrayList<>();
         flights.add(flight1);
         flights.add(flight2);
+        flights.add(flight3);
+        flights.add(flight4);
 
         return flights;
     }
 
+    @CrossOrigin
     @GetMapping("/seats")
     public List<Map<String, Integer>> getSeats() {
         Random seatTaken = new Random();
-        int randomSeat = seatTaken.nextInt(2);
-
+        
         List<Map<String, Integer>> seats = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
+            int randomSeat = seatTaken.nextInt(2);
             Map<String, Integer> seat = new HashMap<>();
             seat.put("id", i);
             if (i % 4 == 0 || (i - 1) % 4 == 0) {
